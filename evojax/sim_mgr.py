dfec6ab6882f711132f2c0ce6635e821d068d243
Maximilien Le Clei <maximilienleclei@gmail.com>
2022-02-17 08:53:48 -0500

    Removed jit around pmap

--- evojax/sim_mgr.py
+++ evojax/sim_mgr.py
@@ -186,8 +186,8 @@ class SimManager(object):
             step_once_fn=partial(step_once, task=train_vec_task),
             max_steps=train_vec_task.max_steps)
         if self._num_device > 1:
-            self._train_rollout_fn = jax.jit(jax.pmap(
-                self._train_rollout_fn, in_axes=(0, 0, 0, None)))
+            self._train_rollout_fn = jax.pmap(
+                self._train_rollout_fn, in_axes=(0, 0, 0, None))
 
         # Set up validation functions.
         self._valid_reset_fn = valid_vec_task.reset
@@ -196,8 +196,8 @@ class SimManager(object):
             step_once_fn=partial(step_once, task=valid_vec_task),
             max_steps=valid_vec_task.max_steps)
         if self._num_device > 1:
-            self._valid_rollout_fn = jax.jit(jax.pmap(
-                self._valid_rollout_fn, in_axes=(0, 0, 0, None)))
+            self._valid_rollout_fn = jax.pmap(
+                self._valid_rollout_fn, in_axes=(0, 0, 0, None))
 
     def eval_params(self, params: jnp.ndarray, test: bool) -> jnp.ndarray:
         """Evaluate population parameters or test the best parameter.
