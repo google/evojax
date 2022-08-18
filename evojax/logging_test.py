38f99f948975f471c2fc3eca2bfdf5e0b24738de
ben <ben@pop-os.localdomain>
2022-08-18 13:38:13 +0100

    Log tests

--- evojax/logging_test.py
+++ evojax/logging_test.py
@@ -1,9 +1,18 @@
 from util import create_logger
 import logging
 
-# from google.cloud import logging as cloudlogging
-import sys
-sys.stdout.flush()
+# Imports the Cloud Logging client library
+import google.cloud.logging
+
+# Instantiates a client
+client = google.cloud.logging.Client()
+
+# Retrieves a Cloud Logging handler based on the environment
+# you're running in and integrates the handler with the
+# Python logging module. By default this captures all logs
+# at INFO level and higher
+client.setup_logging()
+
 
 if __name__ == "__main__":
     log_dir = "./test_logs"
