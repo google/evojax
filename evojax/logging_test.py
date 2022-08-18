845eb622ceba1c8dfe685a8d23d06648b6d8a74e
ben <ben@pop-os.localdomain>
2022-08-18 13:33:11 +0100

    Log tests

--- evojax/logging_test.py
+++ evojax/logging_test.py
@@ -1,6 +1,8 @@
 from util import create_logger
 import logging
 
+from google.cloud import logging as cloudlogging
+
 
 if __name__ == "__main__":
     log_dir = "./test_logs"
@@ -14,5 +16,14 @@ if __name__ == "__main__":
     logging.info('EvoJAX Logging Test')
     logging.info('Is it working now??')
     logging.debug('Debug test....')
+
+    lg_client = cloudlogging.Client()
+
+    lg_handler = lg_client.get_default_handler()
+    cloud_logger = logging.getLogger("cloudLogger")
+    cloud_logger.setLevel(logging.INFO)
+    cloud_logger.addHandler(lg_handler)
+    cloud_logger.info("test out logger carrying normal news")
+    cloud_logger.error("test out logger carrying bad news")
 
 
