19bd11cd58f7e7b35d68893a6bf70e098ba5bd97
ben <benwoodyear@gmail.com>
2022-09-14 20:36:34 +0100

    drop pdb

--- evojax/policy/mask_policy.py
+++ evojax/policy/mask_policy.py
@@ -117,8 +117,8 @@ class MaskPolicy(PolicyNetwork):
                     t_states: State,
                     params: jnp.ndarray,
                     p_states: MaskPolicyState) -> Tuple[jnp.ndarray, PolicyState]:
-        # import ipdb
-        # ipdb.set_trace()
+        import ipdb
+        ipdb.set_trace()
 
         params = self._format_params_fn(params)
         masking_output = self._forward_fn(params, t_states.obs)
