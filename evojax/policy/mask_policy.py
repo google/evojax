fc71788077010f068085fb2b9486484c6b39ca32
ben <benwoodyear@gmail.com>
2022-09-15 11:47:56 +0100

    remove tile of cnn_params

--- evojax/policy/mask_policy.py
+++ evojax/policy/mask_policy.py
@@ -110,7 +110,7 @@ class MaskPolicy(PolicyNetwork):
         keys = jax.random.split(jax.random.PRNGKey(0), states.obs.shape[0])
 
         flat_params = self.flatten_params(self.cnn_state.params)
-        flat_params = jnp.tile(flat_params, jax.local_device_count())
+        # flat_params = jnp.tile(flat_params, jax.local_device_count())
 
         return MaskPolicyState(keys=keys,
                                cnn_params=flat_params)
