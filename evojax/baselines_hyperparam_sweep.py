4d34be99ef10f292c980c5100277206f0d7e4049
ben <benwoodyear@gmail.com>
2022-09-18 15:01:53 +0100

    differentiate baseline types

--- evojax/baselines_hyperparam_sweep.py
+++ evojax/baselines_hyperparam_sweep.py
@@ -17,6 +17,7 @@ def parse_args():
     parser.add_argument('--dropout', action='store_true', help='Test dropout rate.')
 
     # General params
+    parser.add_argument('--trial-count', type=int, default=5, help='How many trials to run.')
     parser.add_argument('--seed', type=int, default=0, help='Random seed for training.')
 
     parsed_config, _ = parser.parse_known_args()
@@ -31,10 +32,24 @@ if __name__ == "__main__":
         os.makedirs(log_dir, exist_ok=True)
     logger = create_logger(name='SWEEP', log_dir=log_dir, debug=False)
 
+    if config.use_task_labels:
+        baseline_type = 'task_labels'
+    elif config.l1_reg:
+        baseline_type = 'l1_reg'
+    elif config.l1_pruning:
+        baseline_type = 'l1_pruning'
+    elif config.dropout:
+        baseline_type = 'dropout'
+    else:
+        baseline_type = None
+
+    # Ensure only one baseline type is being tested
+    assert sum([config.use_task_labels, config.l1_reg, config.l1_pruning, config.dropout]) < 2
+
     seed = 0
     datasets_tuple = full_data_loader()
     study = optuna.create_study(direction="maximize",
-                                study_name=f"mnist_baselines_seed_{seed}",
+                                study_name=f"mnist_baselines_{baseline_type}_seed_{seed}",
                                 storage=f'sqlite:///{log_dir}/optuna_hparam_search.db',
                                 load_if_exists=True)
 
@@ -68,7 +83,7 @@ if __name__ == "__main__":
             l1_pruning_proportion = trial.suggest_float("l1_pruning_proportion", 0.05, 0.8, step=0.05, log=False)
             base_config.update({"l1_pruning_proportion": l1_pruning_proportion})
         if config.dropout:
-            dropout_rate = trial.suggest_float("dropout_rate", 0.05, 0.5, step=0.05, log=False)
+            dropout_rate = trial.suggest_float("dropout_rate", 0.05, 0.7, step=0.05, log=False)
             base_config.update({"dropout_rate": dropout_rate})
 
         cnn_state, accuracy_dict = run_mnist_training(**base_config)
