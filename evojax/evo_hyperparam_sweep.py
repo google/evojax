640e18846441bb50369bfda1a40f5c47b10db677
ben <benwoodyear@gmail.com>
2022-09-19 12:50:36 +0100

    increase evo sweep pop sizes

--- evojax/evo_hyperparam_sweep.py
+++ evojax/evo_hyperparam_sweep.py
@@ -63,15 +63,15 @@ if __name__ == "__main__":
 
         test_params = dict(
             algo=trial.suggest_categorical("algo", ["PGPE", "OpenES"]),
-            pop_size=trial.suggest_categorical("pop_size", [8, 16, 32]),
-            mask_threshold=trial.suggest_float("mask_threshold", 0.3, 0.7),
+            pop_size=trial.suggest_categorical("pop_size", [16, 32, 64, 128]),
+            mask_threshold=trial.suggest_float("mask_threshold", 0.4, 0.6),
             max_iter=trial.suggest_int("max_iter", 20, 300, log=True),
             evo_epochs=trial.suggest_int("evo_epochs", 0, 10, log=False),
             cnn_epochs=trial.suggest_int("cnn_epochs", 1, 5, log=False),
             test_interval=trial.suggest_int("test_interval", 5, 20, log=False),
             center_lr=trial.suggest_float("center_lr", 0, 0.1),
             std_lr=trial.suggest_float("std_lr", 0, 0.2),
-            init_std=trial.suggest_float("init_std", 0, 0.1),
+            init_std=trial.suggest_float("init_std", 0, 0.2),
         )
         params_dict.update(test_params)
 
