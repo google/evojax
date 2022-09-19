80d49a77c2bf7c408c97e0019e8f19f6c150d453
ben <benwoodyear@gmail.com>
2022-09-19 18:53:09 +0100

    drop pdb

--- evojax/task/masking_task.py
+++ evojax/task/masking_task.py
@@ -92,6 +92,9 @@ class Masking(VectorizedTask):
             self.obs_shape = tuple([1, ])
         self.act_shape = tuple([10, ])
 
+        import ipdb
+        ipdb.set_trace()
+        
         if not test:
             dataset_class = datasets_tuple[int(validation)]
             image_data, class_labels, task_labels = dataset_class.return_data_arrays()
