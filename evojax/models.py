9d209d1efa4a34729154d796a9238b277ca3b0b0
ben <benwoodyear@gmail.com>
2022-09-20 12:25:23 +0100

    different names for layers

--- evojax/models.py
+++ evojax/models.py
@@ -67,8 +67,8 @@ class Mask(nn.Module):
     def __call__(self, x):
         x = nn.one_hot(x, self.dataset_number)
 
-        x = nn.Dense(features=10, name=mask_final_layer_name)(x)
-        x = nn.Dense(features=100, name=mask_final_layer_name)(x)
+        x = nn.Dense(features=10, name="LAYER1")(x)
+        x = nn.Dense(features=100, name="LAYER2")(x)
         x = nn.Dense(features=self.mask_size, name=mask_final_layer_name)(x)
         x = nn.sigmoid(x)
 
