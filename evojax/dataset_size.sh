dd2c82f7e9239c91ebdf4b9ee5dfb093870c6b5e
ben <benwoodyear@gmail.com>
2022-09-23 16:06:08 +0100

    testing different dataset sizes with masking

--- evojax/dataset_size.sh
+++ evojax/dataset_size.sh
@@ -1,8 +1,9 @@
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.2
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.3
+python3 train_masking.py --cnn-epochs 20 --algo OpenES --test-interval 100 --log-interval 50 --max-iter 1000 \
+--max-steps 100 --mask-threshold 0.5 --val-fraction 0.4
 python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.4
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.5
+python3 train_masking.py --cnn-epochs 20 --algo OpenES --test-interval 100 --log-interval 50 --max-iter 1000 \
+--max-steps 100 --mask-threshold 0.5 --val-fraction 0.6
 python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.6
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.7
+python3 train_masking.py --cnn-epochs 20 --algo OpenES --test-interval 100 --log-interval 50 --max-iter 1000 \
+--max-steps 100 --mask-threshold 0.5 --val-fraction 0.8
 python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.9
