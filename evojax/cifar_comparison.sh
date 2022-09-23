794408fa2988c9979a7de8d826d0ec6668d0ee0c
ben <benwoodyear@gmail.com>
2022-09-23 13:36:28 +0100

    cifar comparison script

---
+++ evojax/cifar_comparison.sh
@@ -0,0 +1,8 @@
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR KMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST KMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR KMNIST MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST KMNIST MNIST
