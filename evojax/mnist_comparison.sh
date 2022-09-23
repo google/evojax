b5acdbf00516999b0fedd1d249dae1da7525f781
ben <benwoodyear@gmail.com>
2022-09-23 13:34:36 +0100

    mnist comparison script

---
+++ evojax/mnist_comparison.sh
@@ -0,0 +1,8 @@
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST FMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST KMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST CIFAR
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST FMNIST KMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST FMNIST CIFAR
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST KMNIST CIFAR
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets MNIST FMNIST KMNIST CIFAR
