59f91802435109d85b9423c8fa2fad3a6f01fd5b
ben <benwoodyear@gmail.com>
2022-09-23 15:35:06 +0100

    high val fraction for cifar

--- evojax/cifar_comparison.sh
+++ evojax/cifar_comparison.sh
@@ -1,8 +1,8 @@
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR KMNIST
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR MNIST
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST KMNIST
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST MNIST
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR KMNIST MNIST
-python3 train_masking.py --cnn-epochs 20 --max-iter 0 --datasets CIFAR FMNIST KMNIST MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR FMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR KMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR FMNIST KMNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR FMNIST MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR KMNIST MNIST
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8 --datasets CIFAR FMNIST KMNIST MNIST
