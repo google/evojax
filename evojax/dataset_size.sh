a89c96996f1806465be64604a4f712dc6402abaf
ben <benwoodyear@gmail.com>
2022-09-23 15:45:18 +0100

    dataset size tests script

---
+++ evojax/dataset_size.sh
@@ -0,0 +1,8 @@
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.2
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.3
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.4
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.5
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.6
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.7
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.8
+python3 train_masking.py --cnn-epochs 20 --max-iter 0 --val-fraction 0.9
