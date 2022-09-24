5786d54efc78ad6ad853f58cf538a1d518080201
ben <benwoodyear@gmail.com>
2022-09-24 20:37:30 +0100

    fixing baseline results script

--- evojax/scripts/baselines_results.py
+++ evojax/scripts/baselines_results.py
@@ -4,7 +4,7 @@ import argparse
 import pandas as pd
 from evojax.train_masking import run_train_masking
 from google.cloud import storage
-from evojax.datasets import full_data_loader
+from evojax.datasets import full_data_loader, DATASET_LABELS
 from evojax.util import create_logger
 
 
@@ -13,6 +13,7 @@ def parse_args():
 
     parser.add_argument('--number-of-seeds', type=int, default=5, help='How many seeds to test.')
     parser.add_argument('--epochs', type=int, default=40, help='Number of epochs.')
+    parser.add_argument('--early-stopping-count', type=int, help='Number of epochs.')
 
     parsed_config, _ = parser.parse_known_args()
     return parsed_config
@@ -35,18 +36,19 @@ if __name__ == "__main__":
 
     client = storage.Client()
     bucket = client.get_bucket("evojax-bucket")
-    file_name = f'results_run_{time.strftime("%m%d_%H%M")}.csv'
+    file_name = f'baselines{"_early_stopping" if config.early_stopping_count else ""}_{time.strftime("%m%d_%H%M")}.csv'
     file_path = os.path.join(log_dir, file_name)
 
     number_of_seeds = config.number_of_seeds
-    datasets_tuple = full_data_loader()
+    datasets = list(DATASET_LABELS.keys())
+    datasets_tuple = full_data_loader(dataset_names=datasets)
 
-    baseline_dict = dict(
+    baseline_dict = dict(datasets_names=datasets,
         batch_size=1024,
         cnn_epochs=config.epochs,
-        evo_epochs=0,
+        max_iter=0,
         cnn_lr=1e-3,
-        early_stopping=False,
+        early_stopping_count=config.early_stopping_count,
         datasets_tuple=datasets_tuple,
         logger=logger
     )
@@ -55,40 +57,40 @@ if __name__ == "__main__":
     baseline_results = run_and_format_results(baseline_dict, 'baseline')
     full_results.update(baseline_results)
 
-    # task_labels_dict = dict(**baseline_dict, use_task_labels=True)
-    # task_labels_results = run_and_format_results(task_labels_dict, 'task_labels')
-    # full_results = {**full_results, **task_labels_results}
+    task_labels_dict = dict(**baseline_dict, use_task_labels=True)
+    task_labels_results = run_and_format_results(task_labels_dict, 'task_labels')
+    full_results = {**full_results, **task_labels_results}
 
     dropout_dict = dict(**baseline_dict, dropout_rate=0.5)
     dropout_results = run_and_format_results(dropout_dict, 'dropout')
     full_results.update(dropout_results)
 
-    # l1_reg_dict = dict(**baseline_dict, l1_reg_lambda=3e-5)
-    # l1_reg_results = run_and_format_results(l1_reg_dict, 'l1_reg')
-    # full_results = {**full_results, **l1_reg_results}
+    l1_reg_dict = dict(**baseline_dict, l1_reg_lambda=3e-5)
+    l1_reg_results = run_and_format_results(l1_reg_dict, 'l1_reg')
+    full_results = {**full_results, **l1_reg_results}
+
+    l1_pruning_dict = dict(**baseline_dict, l1_pruning_proportion=0.05)
+    l1_pruning_results = run_and_format_results(l1_pruning_dict, 'l1_pruning')
+    full_results = {**full_results, **l1_pruning_results}
+
+    # masking_params = dict(algo="OpenES",
+    #                       pop_size=16,
+    #                       mask_threshold=0.51,
+    #                       max_iter=72,
+    #                       test_interval=16,
+    #                       log_interval=1000,
+    #                       center_lr=0.0825,
+    #                       std_lr=0.08,
+    #                       init_std=0.045)
+    # masking_dict = dict(**baseline_dict, **masking_params)
+    # masking_dict["cnn_epochs"] = 5
+    # masking_dict["evo_epochs"] = 8
+    # masking_results = run_and_format_results(masking_dict, 'masking')
+    # full_results.update(masking_results)
     #
-    # l1_pruning_dict = dict(**baseline_dict, l1_pruning_proportion=0.05)
-    # l1_pruning_results = run_and_format_results(l1_pruning_dict, 'l1_pruning')
-    # full_results = {**full_results, **l1_pruning_results}
-
-    masking_params = dict(algo="OpenES",
-                          pop_size=16,
-                          mask_threshold=0.51,
-                          max_iter=72,
-                          test_interval=16,
-                          log_interval=1000,
-                          center_lr=0.0825,
-                          std_lr=0.08,
-                          init_std=0.045)
-    masking_dict = dict(**baseline_dict, **masking_params)
-    masking_dict["cnn_epochs"] = 5
-    masking_dict["evo_epochs"] = 8
-    masking_results = run_and_format_results(masking_dict, 'masking')
-    full_results.update(masking_results)
-
-    masking_dict["dropout_rate"] = 0.55
-    masking_with_dropout_results = run_and_format_results(masking_dict, 'masking_with_dropout')
-    full_results.update(masking_with_dropout_results)
+    # masking_dict["dropout_rate"] = 0.55
+    # masking_with_dropout_results = run_and_format_results(masking_dict, 'masking_with_dropout')
+    # full_results.update(masking_with_dropout_results)
 
     try:
         series_dict = {k: pd.Series(v) for k, v in full_results.items()}
