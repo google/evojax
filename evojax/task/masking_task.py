3bf3f3103cdad2e35318828529ee39d68f735a6b
ben <benwoodyear@gmail.com>
2022-09-24 22:34:04 +0100

    fix masks in task

--- evojax/task/masking_task.py
+++ evojax/task/masking_task.py
@@ -156,7 +156,7 @@ class Masking(VectorizedTask):
                 return cnn_state, logits
 
             cnn_data = state.cnn_data
-            # masks = action.reshape(self.mask_shape)
+            masks = action
             new_cnn_state, step_logits = train_step(cnn_data.cnn_state, cnn_data.obs, masks, cnn_data.labels)
 
             next_key, key = random.split(state.key)
